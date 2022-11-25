0b0537148714fb73473a3067405728caaf10a540
davidlacayo2 <davidlacayo2@gmail.com>
2022-11-25 12:33:46 -0600

    Worked on finishing tests directory-- make works for objects inside of directory, but for some reason, objects outside of directory have issues

---
+++ tests/tests.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "../src/Graph.h"
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+    cout << "This is our Test File: " << endl;
+}
---
+++ tests/tests.md
@@ -0,0 +1,4 @@
+Tests Directory contains: 
+    - More complex tests for our BFS, Dijkstra and Pagerank algorithm
+
+- To run: "make" in terminal to compile, and "./test" to run 
