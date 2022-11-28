9174443ff8ea20884ec38b4458ed1c2d6e9ed0e6
davidlacayo2 <davidlacayo2@gmail.com>
2022-11-28 16:31:31 -0600

    Fooling around with make files

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -7,4 +7,20 @@ using std::endl;
 
 int main() {
     cout << "This is our Test File: " << endl;
+    //Testing BFS
+    // cout<<"Testing BFS: " << endl;
+    // int vertices = 5;
+    // Graph g1(vertices);
+    // g1.addEdge(0, 1, 1); //node 1, node 2, edge weight
+    // g1.addEdge(0, 2, 7);
+    // g1.addEdge(1, 2, 5);
+    // g1.addEdge(1, 4, 4);
+    // g1.addEdge(4, 3, 2); //currently not working for this node: why? 
+    // g1.addEdge(2, 3, 6);
+ 
+    // int src = 2, dest = 3;
+    
+    // cout << "Graph 1: "<< endl;
+    // cout << "\nShortest Distance between " << src << " and " << dest << " is " << g1.findShortestPathBFS(src, dest) << endl;
+
 }
