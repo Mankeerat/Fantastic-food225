123b845a5305b5cf0f6d7dbe386f750fb5715af5
Jason <jasonzh2@illinois.edu>
2022-12-11 02:43:56 +0000

    Actually Moved Files into New Directory

--- src/Utils.h
+++ src/Utils.h
@@ -10,19 +10,7 @@
 #include <map>
 #include <utility>
 
-using std::map;
-using std::multimap;
-using std::pair;
-using std::vector;
-using std::list;
-using std::stack;
-using std::queue;
-using std::string;
-using std::ifstream;
-using std::cout;
-using std::cin;
-using std::endl;
-
+using namespace std;
 
 map<string, int> mapDistanceGraph(vector<int> & v, map<int, string> & m);
 void printDistanceMap(map<string, int> & m);
